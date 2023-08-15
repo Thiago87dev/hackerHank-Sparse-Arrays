9d55c20b9d069b1dd548cb449aebc4eb9e4a6528
thiago alves <thiago87gsa@gmail.com>
2023-08-15 11:26:05 -0300

    add comment

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+
 function matchingStrings(strings, queries) {
     let r = []
     for(let i of queries){
